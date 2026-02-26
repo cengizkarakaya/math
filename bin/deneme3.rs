abaeaeb5ee11faa935f88d1044d5facadaee84ed
cengizkarakaya <cengizkarakaya@hotmail.com>
2026-02-26 17:46:10 +0300

    .

--- bin/deneme3.rs
+++ bin/deneme3.rs
@@ -3,29 +3,13 @@ use lib::{alt_cizgi, ust_cizgi};
 fn main() {
     ust_cizgi();
 
-    trait UcgenAlan {
-        fn ucgen_alani(&self) -> f64;
+    fn ucgen_alani(x: f64) -> f64 {
+        (3.0f64).sqrt() / 4.0 * x * x
     }
 
-    //#[derive(Debug)]
-    struct Ucgen {
-        kenar: f64,
-    }
+    let sonuc = ucgen_alani(5.0);
 
-    impl UcgenAlan for Ucgen {
-        fn ucgen_alani(&self) -> f64 {
-            (3.0f64).sqrt() / 4.0 * self.kenar * self.kenar
-        }
-    }
-
-    let x = Ucgen { kenar: 5.0 };
-
-    let sonuc = x.ucgen_alani();
-
-    println!(
-        "Eşkenar üçgen kenarı {:?} için:\nÜçgen alanı = {:.2}",
-        x.kenar, sonuc
-    );
+    println!("{:.3}", sonuc);
 
     alt_cizgi();
 }
