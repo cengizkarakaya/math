9db9762f01217c06968262a5a7d2949f26961ef9
cengizkarakaya <cengizkarakaya@hotmail.com>
2026-03-02 13:35:17 +0300

    .

--- bin/deneme3.rs
+++ bin/deneme3.rs
@@ -6,15 +6,27 @@ fn main() {
 
     println!("bir cümle girin:\n");
     io::stdout().flush().unwrap();
+
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("girdi okunamadı!");
 
-    let input =input.trim();
-    println!("{}", input);
-
     let kar_say = input.chars().count();
-    println!("{}", kar_say);
+    println!("\nkarakter sayısı: {}", kar_say);
 
+    let input = input.trim_end_matches(&['\r', '\n'][..]);
+    let bosluk_sayisi = input.chars().filter(|c| c.is_whitespace()).count();
+    println!("Toplam boşluk sayısı: {}", bosluk_sayisi);
+    println!("boşluksuz karakter sayısı: {}", kar_say - bosluk_sayisi);
+
+    let kelime_sayisi = input.split_whitespace().count();
+    println!("kelime sayısı: {}", kelime_sayisi);
+
+    let en_uzun = input.split_whitespace().max_by_key(|w| w.chars().count());
+
+    match en_uzun {
+        Some(kelime) => println!("En uzun kelime: {}", kelime),
+        None => println!("Kelime yok"),
+    }
 
     alt_cizgi();
 }
